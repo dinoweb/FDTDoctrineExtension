300d9bc7163b224bbf8d8f59669cb43d394a2385
lorenzo.caldara <lorenzo.caldara@gmail.com>
2012-04-02 19:14:52 +0000

    Modificata compatibilità con Doctrine 2.2

--- lib/FDT/doctrineExtensions/NestedSet/TreeManager.php
+++ lib/FDT/doctrineExtensions/NestedSet/TreeManager.php
@@ -3,7 +3,7 @@
 namespace FDT\doctrineExtensions\NestedSet;
 
 use  FDT\doctrineExtensions\NestedSet\Documents\BaseNode;
-use  Doctrine\ODM\MongoDB\DocumentManager;
+use  Doctrine\ODM\MongoDB\ManagerRegistry;
 
 
 class TreeManager
@@ -14,86 +14,86 @@ class TreeManager
 
 	public function __construct(DocumentManager $documentManager)
 	{
-	
+
 		$this->documentManager = $documentManager;
-	
+
 	}
-	
+
 	public function getDocumentManager ()
 	{
 		return $this->documentManager;
 	}
 
-	
-	
+
+
 	public function getNode (BaseNode $document)
 	{
 		$objectId = spl_object_hash($document);
-		
+
 		if(!isset($this->nodes[$objectId]) || $this->nodes[$objectId]->getDocument() !== $document)
         {
-        
+
         	$this->nodes[$objectId] =  new NodeWrapper ($document, $this->getDocumentManager ());
-        
-        
+
+
         }
-	    		
+
     	return $this->nodes[$objectId];
-	
+
 	}
-	
+
 	public function getRoots ($document, $level = 0, $return = 'array')
 	{
-	
+
 		$qb =  $this->getDocumentManager()->createQueryBuilder($document);
 		$qb = $qb->field('level')->equals(0);
-				
+
 		$query = $qb->getQuery();
-		
+
 		$results = $query->execute();
-		
+
 		switch ($return)
 		{
 			case 'objects':
         		return $results;
         	break;
-        	
+
         	case 'array':
-        	
+
         		$arrayRoots = array();
-        		
+
         		foreach ($results as $rootDocument)
         		{
-        		
+
         			$arrayRoots[] = $this->getTreeAsArray ($rootDocument, $includeRoot = TRUE, $level);
-        		
+
         		}
-        		
+
         		return($arrayRoots);
-        		
+
         	break;
-		
-		
+
+
 		}
-		
-		
-	
+
+
+
 	}
-	
-	
+
+
 	public function getTreeAsArray (BaseNode $document, $includeRoot = TRUE, $level = NULL)
 	{
 		$arrayTree = array ();
 		$collection = NULL;
-		
+
 		$node = $this->getNode ($document);
 		$collection = 	$node->getDescendants ($level);
-			
+
 		if ($includeRoot)
 		{
 			$arrayTree[] = $document->getStringForPath();
 		}
-						
+
 		if ($collection->count () > 0)
 		{
 			foreach ($collection as $document)
@@ -101,50 +101,50 @@ class TreeManager
 				$arrayTree[] = $document->getStringForPath();
 			}
 		}
-			
+
 		return $arrayTree;
-		
-		
-		
-	
+
+
+
+
 	}
-	
+
 	private function parentIsRoot ($parentId)
 	{
-	   
+
 	   $rootStringNumber =  preg_match('/^idRoot/', $parentId);
-                                                                    
+
        if ($rootStringNumber > 0)
        {
-         return true; 
+         return true;
        }
 
        return false;
-	
+
 	}
-		
+
 	public function manageTreeMovements($document, array $requestData, $repository)
     {
-                
+
         if ($this->parentIsRoot ($requestData['parentId']))
         {
-            
+
             $nodeDocument = $this->getNode ($document);
             $nodeDocument->setAsRoot ();
-            
+
         }
         else
         {
-           
+
             $parentRecord = $repository->getByMyUniqueId ($requestData['parentId'], 'id');
-                
+
             $parentNode = $this->getNode ($parentRecord);
-        
+
             $parentNode->addChild ($document);
         }
-            
-            
-            return $document;    
+
+
+            return $document;
     }
 
 
--- test/FDT/doctrineExtensions/Nestedset/NodeTest.php
+++ test/FDT/doctrineExtensions/Nestedset/NodeTest.php
@@ -23,7 +23,7 @@ class NestedsetTest extends \PHPUnit_Framework_TestCase
     private $documentManager;
 
     public function setUp()
-    {        
+    {
         $config = new \Doctrine\ODM\MongoDB\Configuration();
         $config->setProxyDir(__DIR__ . '/Proxy');
         $config->setProxyNamespace('dinoWeb\Nestedset\Proxy');
@@ -31,143 +31,143 @@ class NestedsetTest extends \PHPUnit_Framework_TestCase
         $config->setHydratorNamespace('dinoWeb\Nestedset\Hydrator');
         $config->setDefaultDB('dinoWeb_Nestedset_test');
         $config->setAutoGenerateProxyClasses(true);
-                
+
         $this->documentManager = $this->getTestDocumentManager ($config);
-        
-                
-        
+
+
+
     }
-    
+
     public function testNodeWrapperCreation ()
     {
-    
+
     	$mobili = new Categoria ();
        	$mobili->setName ('Mobili');
        	$mobili->setEbayId (200);
        	$mobili->getEbayOfferedEnable (1);
-       	       	
+
        	$nodeRoot = $this->getTreeManager ()->getNode ($mobili);
-       	
+
        	$this->assertEquals ('FDT\doctrineExtensions\NestedSet\NodeWrapper', get_class($nodeRoot));
-    
-    
+
+
     }
-    
+
     public function testAddChild ()
     {
-    	
+
     	$categoria1 = new Categoria ();
        	$categoria1->setName ('Mobili');
        	$categoria1->setEbayId (200);
        	$categoria1->getEbayOfferedEnable (1);
-       	
+
        	$categoria2 = new Categoria ();
        	$categoria2->setName ('Sedie');
        	$categoria2->setEbayId (300);
        	$categoria2->getEbayOfferedEnable (1);
-       	
+
        	$nodeRoot = $this->getTreeManager ()->getNode ($categoria1);
-              	       	
+
        	$nodeRoot->addChild ($categoria2);
-       	
+
        	$this->documentManager->persist($categoria2);
        	$this->documentManager->flush ();
-       	
+
        	$this->assertEquals (1, $categoria2->getLevel());
-       	
+
        	$this->assertEquals (1, count ($categoria2->getAncestors()));
-       	
-       	
+
+
        	$descendants = $nodeRoot->getDescendants ();
-       	
+
        	$this->assertEquals (1, $descendants->count());
-       	
+
        	$categoria3 = new Categoria ();
        	$categoria3->setName ('Sgabelli');
        	$categoria3->setEbayId (400);
        	$categoria3->getEbayOfferedEnable (1);
-       	
+
        	$nodeRoot->addChild ($categoria3);
-       	
+
        	$this->documentManager->persist($categoria3);
        	$this->documentManager->flush ();
-       	
+
        	$descendants = $nodeRoot->getDescendants ();
-       	
+
        	$this->assertEquals (2, $descendants->count());
-    
-    
+
+
     }
-    
+
     public function testNodeWrapper ()
     {
-    
+
     	$mobili = $this->getCategoria('Mobili');
        	$sedie = $this->getCategoria('Sedie');
-       	
+
        	$nodeRoot = $this->getTreeManager ()->getNode ($mobili);
-              	       	
+
        	$nodeRoot->addChild ($sedie);
-       	
+
        	$this->documentManager->persist($sedie);
        	$this->documentManager->flush ();
-       	
+
        	$sgabelli = $this->getCategoria('Sgabelli');
-       	
+
        	$nodeRoot->addChild ($sgabelli);
-       	
+
        	$this->documentManager->persist($sgabelli);
        	$this->documentManager->flush ();
-       	
+
        	$sgabelliAlti = $this->getCategoria('Sgabelli Alti');
-       	       	
+
        	$nodeSgabelli = $this->getTreeManager ()->getNode ($sgabelli);
-       	
+
        	$nodeSgabelli->addChild ($sgabelliAlti);
-       	
+
        	$this->documentManager->persist($sgabelliAlti);
        	$this->documentManager->flush ();
-       	       	       	
+
        	$this->assertEquals (1, $nodeSgabelli->getLevel());
-       	
+
        	$nodeSgabelliAlti = $this->getTreeManager ()->getNode ($sgabelliAlti);
-       	
+
        	$this->assertEquals (2, $nodeSgabelliAlti->getLevel());
-       	
+
        	$arrayTree =  ($this->getTreeManager ()->getTreeAsArray ($mobili));
-       	
+
        	$this->assertEquals (4, count ($arrayTree));
-       	
-       	
+
+
        	$nodeSgabelliAlti = $this->getTreeManager ()->getNode ($sgabelliAlti);
 		$pathSgabelliAlti = $nodeSgabelliAlti->getPath();
-		
+
 		$this->assertEquals ('Mobili_Sgabelli_Sgabelli Alti', $pathSgabelliAlti);
-		
+
 		$pathRoot = $nodeRoot->getPath();
-		
+
 		$this->assertEquals ('Mobili', $pathRoot);
-    	
+
     	$arrayTree =  ($this->getTreeManager ()->getTreeAsArray ($mobili, TRUE, 3));
-    	
-    	
+
+
     }
-    
-	
+
+
 	private function getCategoria ($nome)
 	{
-		
+
 		$categoria = new Categoria ();
        	$categoria->setName ($nome);
        	$categoria->setEbayId (200);
        	$categoria->getEbayOfferedEnable (1);
-       	
+
        	return $categoria;
-	
-	
+
+
 	}
-    
-    
+
+
     private function getTestDocumentManager($configuration)
     {
 
@@ -175,29 +175,29 @@ class NestedsetTest extends \PHPUnit_Framework_TestCase
         $reader->setDefaultAnnotationNamespace('Doctrine\ODM\MongoDB\Mapping\\');
         $annotationDriver = new AnnotationDriver($reader, __DIR__ . '/../Documents');
         $configuration->setMetadataDriverImpl($annotationDriver);
-                
+
         $mongoClass = new Mongo('int-app-glusterfs.gemmyx.com:8080', array('persist'=>'x'));
-        
+
         $eventManager = new EventManager();
 		$eventManager->addEventSubscriber(new NestedSetListener());
-        
+
 		return DocumentManager::create(new Connection($mongoClass), $configuration, $eventManager);
 
     }
-    
+
     private function getTreeManager ()
     {
-    	
-    	return new TreeManager ($this->documentManager);    
-    
+
+    	return new TreeManager ($this->documentManager);
+
     }
-    
+
     private function getTree ()
     {
-    
-    
-    
-    
+
+
+
+
     }
-    
+
 }
